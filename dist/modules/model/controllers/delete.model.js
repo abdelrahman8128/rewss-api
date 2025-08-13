ab924839eb2e5c3670e230d0c777c862e36273c3
abdelrahman8128 <abdelrahmank8128@gmail.com>
2025-08-13 16:03:21 +0300

    refactor: simplify delete model controller by removing error handling block

--- dist/modules/model/controllers/delete.model.js
+++ dist/modules/model/controllers/delete.model.js
@@ -9,21 +9,13 @@ const http_status_codes_1 = require("http-status-codes");
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
 exports.deleteModel = (0, express_async_handler_1.default)(async (req, res) => {
     const modelService = new model_service_1.ModelService();
-    try {
-        const deletedModel = await modelService.deleteModel(req);
-        if (deletedModel === false) {
-            res.status(http_status_codes_1.StatusCodes.NOT_FOUND).json({ message: "Model not found" });
-        }
+    const deletedModel = await modelService.deleteModel(req);
+    if (deletedModel === false) {
+        res.status(http_status_codes_1.StatusCodes.NOT_FOUND).json({ message: "Model not found" });
+    }
+    else {
         res.status(http_status_codes_1.StatusCodes.OK).json({
             message: "Model deleted successfully",
         });
-    }
-    catch (error) {
-        if (error instanceof Error) {
-            res.status(http_status_codes_1.StatusCodes.BAD_REQUEST).json({ message: error.message });
-        }
-        else {
-            res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "An unexpected error occurred" });
-        }
     }
 });
--- src/modules/model/controllers/delete.model.ts
+++ src/modules/model/controllers/delete.model.ts
@@ -6,19 +6,12 @@ import asyncHandler from "express-async-handler";
 export const deleteModel = asyncHandler(async (req: Request, res: Response) => {
   const modelService = new ModelService();
 
-  try {
     const deletedModel = await modelService.deleteModel(req);
     if (deletedModel === false) {
        res.status(StatusCodes.NOT_FOUND).json({ message: "Model not found" });
-    }
+    }else{
     res.status(StatusCodes.OK).json({
       message: "Model deleted successfully",
     });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
-    } else {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "An unexpected error occurred" });
-    }
   }
 });
