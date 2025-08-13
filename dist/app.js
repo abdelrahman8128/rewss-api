0320ed451ea7cb0d4d84a12398e5afb9ef673020
abdelrahman8128 <abdelrahmank8128@gmail.com>
2025-08-13 15:51:34 +0300

    feat: add CORS middleware with configuration for all origins and methods

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 require("./config/dotenv.config");
 require("./config/mongodb.config");
 require("reflect-metadata");
@@ -11,6 +12,12 @@ const morgan_1 = __importDefault(require("morgan"));
 const app_route_1 = __importDefault(require("./app.route"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+app.use((0, cors_1.default)({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)("dev"));
 (0, app_route_1.default)(app);
--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 // Handle preflight
-app.options('*', cors());
+app.use(cors());
 
 app.use(express.json());
 
