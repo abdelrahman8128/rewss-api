295eaf6ca25503ba9f8cb1de3642d340b69d123b
abdelrahman8128 <abdelrahmank8128@gmail.com>
2025-08-14 12:38:27 +0300

    fix: update dotenv configuration to always load from .env file

--- src/config/dotenv.config.ts
+++ src/config/dotenv.config.ts
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 import path from "path";
 
 // Load environment variables from .env file
-const env = process.env.NODE_ENV === "production" ? ".env" : ".env.development";
-export default dotenv.config({
+//const env = process.env.NODE_ENV === "production" ? ".env" : ".env.development";
+const env = (process.env.NODE_ENV = ".env");
+
+dotenv.config({
   path: path.resolve(__dirname, `../../${env}`),
 });
